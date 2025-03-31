84b405bd7b92accd1e478acd16b728544d2dab7d
Irfan Mehmud <irfanmehmud140@gmail.com>
2025-03-31 20:46:33 +0530

    Add custom ApiResponse class for structured response handling

---
+++ server/src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+// Custom response for Api
+class ApiResponse {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+
+export { ApiResponse }
