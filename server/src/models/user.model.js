821a4f43cb97e9bfdca3812433b2bfdb56d1bf4a
Irfan Mehmud <irfanmehmud140@gmail.com>
2025-04-01 10:45:53 +0530

    add functions to generate Refresh Token and Access Token

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -54,6 +54,45 @@ const userSchema = new Schema(
     }
 );
 
+// Mongoose pre-save hook middleware function that runs before a userSchema document is saved to the database
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
 
+// Compares the plain-text password with the hashed password stored in the database.
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+//  Generate Access Token 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            name: this.name,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+// Generate Refresh Token
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema)
